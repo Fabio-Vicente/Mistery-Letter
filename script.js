daad0d0582539883a6def7b697faf1337bd1ded0
Fábio Vicente <fab10_lima@hotmail.com>
2021-11-30 11:49:05 -0300

    Requisito 5: Adicionando mensagem de alerta para caso o usuário insira um texto vazio

--- script.js
+++ script.js
@@ -8,14 +8,28 @@ function removeOldLetter() {
     display.removeChild(textDisplay[0]);
   }
 }
+function verifyEmptyText(arrOfWords) {
+  for (let i = 0; i < arrOfWords.length; i += 1) {
+    if (arrOfWords[i] !== '') {
+      return false;
+    }
+  }
+  return true;
+}
 function displayLetter() {
   const letter = inputLetter.value;
   const words = letter.split(' ');
   removeOldLetter();
-  for (let i = 0; i < words.length; i += 1) {
-    const span = document.createElement('span');
-    span.innerText = words[i];
-    display.appendChild(span);
+  display.innerText = '';
+  const empty = verifyEmptyText(words);
+  if (empty) {
+    display.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    for (let i = 0; i < words.length; i += 1) {
+      const span = document.createElement('span');
+      span.innerText = words[i];
+      display.appendChild(span);
+    }
   }
 }
 
